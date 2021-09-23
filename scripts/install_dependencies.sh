fb56f02600a5028b02676b864d3555a855ff0203
dem0n-exe <absolutez3ro.exe@gmail.com>
2021-09-23 15:41:47 +0530

    Fix script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,20 +6,19 @@
 
 # here we update the server and install node and npm
 echo installing dependencies
-sudo yum update
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum -y update
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 sudo yum install nodejs -y
-sudo yum -y install npm
 
 # check to make sure the symbolic link for nodejs node exists
-echo checking for nodejs symlink
-file="/usr/bin/node"
-if [ -f $file ] && [ ! -L $file ] ; then
-  echo "$file exists and is not a symlink"
-  sudo ln -s /usr/bin/nodejs
-else
-  echo "$file exists and is already a symlink"
-fi
+# echo checking for nodejs symlink
+# file="/usr/bin/node"
+# if [ -f $file ] && [ ! -L $file ] ; then
+#   echo "$file exists and is not a symlink"
+#   sudo ln -s /usr/bin/nodejs
+# else
+#   echo "$file exists and is already a symlink"
+# fi
 
 # install the application using npm
 # we need to traverse to where the application bundle is copied too.
